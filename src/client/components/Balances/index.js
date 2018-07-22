b1a0198a168bf002046c26fd20369bb3b013168f
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-07-22 16:55:25 -0700

    [Balances] use Page abstraction

--- src/client/components/Balances/index.js
+++ src/client/components/Balances/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import Typography from 'material-react-components/es/Typography';
 import List from 'material-react-components/es/List';
 import PropTypes from 'prop-types';
 import Form from './Form';
 import BalanceListItem from './BalanceListItem';
+import Page from '../Page';
 
 export default function Balances(props) {
   const { onSubmit, balances } = props;
   return (
-    <div style={{ margin: '20px' }}>
-      <Typography type="display1" style={{ color: '#FFF' }}>
-        Ethereum Wallets
-      </Typography>
+    <Page title="Ethereum Wallets">
       <Form onSubmit={onSubmit} />
       <List style={{ backgroundColor: 'transparent', color: '#30cd9a' }}>
         {balances.map(balance => <BalanceListItem key={balance.id} {...balance} />)}
       </List>
-    </div>
+    </Page>
   );
 }
 
