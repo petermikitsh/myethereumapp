3b8c120c5d5cc433f412b04be1c96087f80d4e8f
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-07-22 16:54:43 -0700

    [BalanceListItem] use ContextProvider

--- src/client/components/Balances/__tests__/BalanceListItem.test.js
+++ src/client/components/Balances/__tests__/BalanceListItem.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import Component from '../BalanceListItem';
+import ContextProvider from '../../../__tests__/ContextProvider';
 
 describe('src/client/components/Balances/BalanceListItem', () => {
   test('BalanceListItem should render without error', () => {
@@ -9,8 +10,12 @@ describe('src/client/components/Balances/BalanceListItem', () => {
       id: '0x7658ef9aa7817e1ba390c5ba3c526115d25452b3',
       updatedAt: '2018-07-22T01:22:08.245Z',
     };
-    const component = TestRenderer.create(<Component {...props} />);
-    component.getInstance().tick();
+    const component = TestRenderer.create(
+      <ContextProvider>
+        <Component {...props} />
+      </ContextProvider>,
+    );
+    component.root.findByType(Component).instance.tick();
     expect(component).toBeTruthy();
   });
 });
