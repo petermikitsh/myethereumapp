714087f6188a0b847b8299b5579b229b3ee3a998
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-07-22 16:57:14 -0700

    [Transactions] route implementation

---
+++ src/client/routes/Transactions.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import Transactions from '../components/Transactions';
+import { API_CLIENT } from '../constants';
+
+class TransactionsRoute extends React.Component {
+  componentDidMount() {
+    const { find } = this.props;
+    find({
+      query: {
+        $sort: {
+          blockNumber: 1,
+        },
+      },
+    });
+  }
+
+  render() {
+    const { transactions } = this.props;
+    return <Transactions transactions={transactions} />;
+  }
+}
+
+TransactionsRoute.propTypes = {
+  transactions: PropTypes.array.isRequired,
+  find: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    transactions: state['api/transactions'].queryResult || [],
+  };
+}
+
+function mapActionsToProps() {
+  return {
+    find: API_CLIENT['api/transactions'].find,
+  };
+}
+
+export default connect(mapStateToProps, mapActionsToProps())(TransactionsRoute);
