1a3c84e542aa6d74753445427316b4e61a3968cf
Peter Mikitsh <peter.mikitsh@gmail.com>
2018-07-22 17:00:40 -0700

    [App] include Transaction routes

--- src/client/app.js
+++ src/client/app.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
-import Root from './routes/Root';
+import Balances from './routes/Balances';
 import AppBar from './components/AppBar';
 import Tabs from './components/Tabs';
+import Transactions from './routes/Transactions';
+import TransactionsByAddress from './routes/TransactionsByAddress';
+
+const Placeholder = ({ children }) => children;
 
 const App = ({ store }) => (
-  [
-    <AppBar key="0" />,
-    <Tabs key="1" />,
-    <Provider key="2" store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Root} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>,
-  ]
+  <Provider store={store}>
+    <BrowserRouter>
+      <Placeholder>
+        <AppBar />
+        <Route component={Tabs} />
+        <Route exact path="/" component={Balances} />
+        <Route exact path="/transactions" component={Transactions} />
+        <Route exact path="/transactions/:id" component={TransactionsByAddress} />
+      </Placeholder>
+    </BrowserRouter>
+  </Provider>
 );
 
 App.propTypes = {
